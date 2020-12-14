832bc48677633bdcc1d8cbcdd622ba765227970e
Ignatius Rahardi <ignatiusrahardi@gmail.com>
2020-12-14 15:26:25 +0700

    init commit flutter project

---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+Map<int, Color> color =
+      {
+      50:Color.fromRGBO(136,14,79, .1),
+      100:Color.fromRGBO(136,14,79, .2),
+      200:Color.fromRGBO(136,14,79, .3),
+      300:Color.fromRGBO(136,14,79, .4),
+      400:Color.fromRGBO(136,14,79, .5),
+      500:Color.fromRGBO(136,14,79, .6),
+      600:Color.fromRGBO(136,14,79, .7),
+      700:Color.fromRGBO(136,14,79, .8),
+      800:Color.fromRGBO(136,14,79, .9),
+      900:Color.fromRGBO(136,14,79, 1),
+      };
+MaterialColor colorCustom = MaterialColor(0xFF5a7e8c, color);
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: colorCustom,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Hello World'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _string = "Welcome!";
+
+  void _setHelloWorld() {
+    setState(() {
+      _string = "Hello World";
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+             '$_string',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            RaisedButton(
+              child: 
+                Text('Press Me To Say Hello World', 
+                style: TextStyle(fontSize: 20),),
+              onPressed: _setHelloWorld,
+              color: Color(0xFF5a7e8c),
+              )
+          ],
+        ),
+      ),
+    );
+  }
+}
